40c53a1004e734978f2d6fee716ec117191568e5
pawelkleczek <pa.vel.kleczek@gmail.com>
2017-06-14 20:21:55 +0200

    http + s

--- js/out01.js
+++ js/out01.js
@@ -1,5 +1,5 @@
 $(() => {
-  var url = 'http://api.themoviedb.org/3/search/movie?api_key=8990436c90718240a2f238e1d25d0526&query=totoro';
+  var url = 'https://api.themoviedb.org/3/search/movie?api_key=8990436c90718240a2f238e1d25d0526&query=totoro';
 
 	$.ajax({
 		url: url,
